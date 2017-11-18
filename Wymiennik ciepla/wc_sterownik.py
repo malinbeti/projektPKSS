b454e3f14b6adc2ae62cbe96263332a1d684f8c1
jarcyk <jarcyk1994@gmail.com>
2017-11-18 11:46:44 +0100

    Implemetacja regulatora wymiennika ciepla

---
+++ Wymiennik ciepla/wc_sterownik.py
@@ -0,0 +1,29 @@
+import pid
+import MySQLdb
+import time
+
+p = pid.PID(5.0, 5.0, 0)  # inicjalizaja regulatora PID
+wait_time = 5000
+
+while True:
+    con = MySQLdb.connect('12.12.12.55', 'pkss', 'pkss125', 'pkss')  # połączenie z bazą
+    cur = con.cursor()
+
+    cur.execute("SELECT value FROM system where name = 'Tzco' ")
+    tzco = cur.fetchall()
+    cur.execute("SELECT value FROM system where name = 'To' ")
+    to = cur.fetchall()
+
+    e = to - 55 - tzco  # uchyb regulacji: różnica pomiędzy zadaną temperaturą wody wypływającej z wymiennika (to-55), a aktualną wartością
+    p.setPoint(to-55.0)
+    um = p.update(e)
+    if um > 60:  # saturacja
+        um = 60
+    if um < 20:
+        um = 20
+
+    update_um = "UPDATE system SET value = %s WHERE name = 'Um'"
+    cur.execute(update_um, um)
+    con.commit()
+    con.close()
+    time.sleep(wait_time)
