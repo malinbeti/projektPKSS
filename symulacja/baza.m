0ae21ec702eee0c2247c0867ffe651492f845a0d
harpun11 <mateusz6556@wp.pl>
2017-11-28 17:44:00 +0100

    Add files via upload

--- symulacja/baza.m
+++ symulacja/baza.m
@@ -1,11 +1,21 @@
-datasource = 'harpun';
-conn = database('harpun','harpun','fWWwZnhvtkN4dSSZ')
+clear all
+close all
+conn = database('pkss','pkss','pkss125',...
+                'Vendor','MySQL',...
+                'Server','12.12.12.55');
 
+curs =exec(conn,'select * from system');
+curs = fetch(curs);
+dane_z_bazy = curs.Data;
 
-datainsert(conn,tablename,colnames,data)
+exec(conn,'UPDATE system SET value = 10 WHERE name = "test"');
 
-%  fetch lub select -- pobieranie
+curs =exec(conn,'select * from system');
+curs = fetch(curs);
+dane_z_bazy2 = curs.Data;
 
-%  datainsert lub update -- wysy³anie
+dane_z_bazy(1,2)
+dane_z_bazy2(1,2)
 
+close(curs);
 close(conn);
