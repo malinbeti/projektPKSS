3f8d9af457a20790b146bc32eba73be776ccfa3b
jaroslaw-b <jarcyk1994@gmail.com>
2017-12-01 11:28:58 +0100

    Dodana synchronizacja za pomocą flagi

--- Wymiennik ciepla/wc_sterownik.py
+++ Wymiennik ciepla/wc_sterownik.py
@@ -4,26 +4,29 @@ import time
 
 p = pid.PID(5.0, 5.0, 0)  # inicjalizaja regulatora PID
 wait_time = 5000
-
+flag = 1
 while True:
+    flag_prev = flag
     con = MySQLdb.connect('12.12.12.55', 'pkss', 'pkss125', 'pkss')  # połączenie z bazą
     cur = con.cursor()
 
-    cur.execute("SELECT value FROM system where name = 'Tzco' ")
-    tzco = cur.fetchall()
-    cur.execute("SELECT value FROM system where name = 'To' ")
-    to = cur.fetchall()
+    cur.execute("SELECT value FROM system where name = 'flaga' ")
+    flag = cur.fetchall()
+    if flag<flag_prev:
+        cur.execute("SELECT value FROM system where name = 'Tzco' ")
+        tzco = cur.fetchall()
+        cur.execute("SELECT value FROM system where name = 'To' ")
+        to = cur.fetchall()
 
-    e = to - 55 - tzco  # uchyb regulacji: różnica pomiędzy zadaną temperaturą wody wypływającej z wymiennika (to-55), a aktualną wartością
-    p.setPoint(to-55.0)
-    um = p.update(e)
-    if um > 60:  # saturacja
-        um = 60
-    if um < 20:
-        um = 20
+        e = to - 55 - tzco  # uchyb regulacji: różnica pomiędzy zadaną temperaturą wody wypływającej z wymiennika (to-55), a aktualną wartością
+        p.setPoint(to-55.0)
+        um = p.update(e)
+        if um > 60:  # saturacja
+            um = 60
+        if um < 20:
+            um = 20
 
-    update_um = "UPDATE system SET value = %s WHERE name = 'Um'"
-    cur.execute(update_um, um)
+        update_um = "UPDATE system SET value = %s WHERE name = 'Um'"
+        cur.execute(update_um, um)
     con.commit()
     con.close()
-    time.sleep(wait_time)
